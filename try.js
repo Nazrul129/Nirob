c623b541958e3bb2d1d567326f334508716c379b
Nazrul129 <bbc.n2000.com@gmail.com>
2025-12-17 06:49:17 +0600

    Add files via upload

---
+++ try.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").onclick = function() {
+    document.getElementById("text").innerText = "Hi 🤗";
+}
+
+
+
+document.getElementById("btn2").onclick = function() {
+    document.getElementById("text").innerText = "Hello";
+}
